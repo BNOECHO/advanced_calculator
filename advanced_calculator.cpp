9aac4df29d987c1359d17bbf77764d97447a0d03
BNOECHO <74729129+BNOECHO@users.noreply.github.com>
2020-11-20 16:45:22 +0800

    Update advanced_calculator.cpp

--- advanced_calculator.cpp
+++ advanced_calculator.cpp
@@ -4,14 +4,21 @@
 #include<math.h>
 #include"advanced_calculator.h"
 using namespace std;
-bool isO(string a)//檢測是否為運算子
+bool isN(char a)//檢測是否為運算子
 {
-	switch (a[0])
+	switch (a)
 	{
-	case '+':
-	case '-':
-	case '*':
-	case '/':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	case '.':
 		return true;
 	}
 	return false;
@@ -41,7 +48,7 @@ double cal(string &a)
 		case '(':
 			if (TEMP != "")F.push_back(TEMP);
 			TEMP = "";
-			if (F.size() >= 1)if (!isO(F[F.size() - 1]))F.push_back("*");
+			if (F.size() >= 1)if (isN(F[F.size() - 1][0]))F.push_back("*");
 
 			F.push_back(to_string(cal(a)));
 			break;
@@ -52,14 +59,59 @@ double cal(string &a)
 			end = true;
 		case ' ':
 			break;
-		default:
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case '.':
 			TEMP += c;
+			break;
+		default://將命令字串儲存(COS SIN....等)
+			TEMP += c;
+			
+
 		}
 
 	}
 
 
 	//讀取到結尾 開始運算
+	//第零階運算(SP函數 COS SIN TAN....ETC)
+	for (int i = 1; i < F.size(); i ++)
+	{
+		if (F[i] == "cos")
+		{
+			F[i] = to_string(cos(stod(F[i + 1])));
+			F.erase(F.begin() + i+1);
+			i--;
+		}
+		if (F[i] == "sin")
+		{
+			F[i] = to_string(sin(stod(F[i + 1])));
+			F.erase(F.begin() + i + 1);
+			i--;
+		}
+		if (F[i] == "tan")
+		{
+			F[i] = to_string(tan(stod(F[i + 1])));
+			F.erase(F.begin() + i + 1);
+			i--;
+		}
+
+
+
+
+
+
+	}
+
+
 	//第一階運算(^)
 	for (int i = 1; i < F.size(); i += 2)
 	{
